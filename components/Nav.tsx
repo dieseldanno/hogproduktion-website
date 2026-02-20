c531c479cce1cbae1c5b0caf0f9199499da94e50
dieseldanno <danno_95@hotmail.com>
2026-02-20 14:08:26 +0100

    ready for take off n2

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -14,24 +14,31 @@ export default function Nav() {
   const isAboutUsPage = pathname === '/om-oss';
 
   return (
-    <>
-      {/* upper header */}
+    <div className="relative w-full">
+      {/* 1. EMAIL MARQUEE */}
       <EmailMarquee />
-      <header className="flex flex-col items-center justify-center px-6 py-1 sm:flex-row sm:items-center sm:justify-between">
-        {/* LOGO */}
+
+      {/* 2. LOGO */}
+      <div className="relative z-50 mt-2 flex justify-center sm:absolute sm:top-0 sm:left-6 sm:justify-start">
         <Link href="/">
           <Image
             src={hogLogo}
             alt="HÖG Produktion"
             width={100}
             height={100}
-            className="h-20 w-auto sm:h-24"
+            className="h-24 w-auto sm:h-26"
             priority
           />
         </Link>
+      </div>
+
+      {/* 3. HEADER */}
+      <header className="mt-4 flex w-full flex-col items-center px-6 py-1 sm:mt-0 sm:flex-row sm:items-start sm:justify-between">
+        {/* LOGO SPACER FÖR CENTRERING */}
+        <div className="hidden sm:block sm:w-1/3"></div>
 
         {/* MENY */}
-        <nav className="mt-4 flex flex-col items-center gap-2 sm:mt-0 sm:flex-row sm:gap-8">
+        <nav className="flex flex-col items-center justify-center gap-2 sm:w-1/3 sm:flex-row sm:gap-8">
           <Link
             href="/aktuellt"
             className={`text-xl font-bold transition-transform duration-300 hover:scale-105 md:text-2xl ${
@@ -42,7 +49,6 @@ export default function Nav() {
           >
             AKTUELLT
           </Link>
-
           <Link
             href="/arkiv"
             className={`text-xl font-bold transition-transform duration-300 hover:scale-105 md:text-2xl ${
@@ -53,7 +59,6 @@ export default function Nav() {
           >
             ARKIV
           </Link>
-
           <Link
             href="/om-oss"
             className={`text-xl font-bold transition-transform duration-300 hover:scale-105 md:text-2xl ${
@@ -67,7 +72,7 @@ export default function Nav() {
         </nav>
 
         {/* SOCIALS */}
-        <div className="mt-4 flex gap-4 sm:mt-0">
+        <div className="mt-4 flex justify-end gap-4 sm:mt-0 sm:w-1/3">
           <Link
             href="https://www.instagram.com/hogproduktion/"
             target="_blank"
@@ -86,6 +91,6 @@ export default function Nav() {
           </Link>
         </div>
       </header>
-    </>
+    </div>
   );
 }
